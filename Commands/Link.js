a09dfb580e97ec255f98b728e87f1b60db47d5a9
doge <shadowphoenixcoc@gmail.com>
2020-08-28 22:36:33 -0400

    Update Link.js

--- Commands/Link.js
+++ Commands/Link.js
@@ -1,5 +1,19 @@
 const Command = require("../Structures/Command");
 const Discord = require("discord.js");
+async function awaitReply(msg, question, limit = 60000) {
+  const filter = (m) => m.author.id === msg.author.id;
+  const qC = await msg.author.send(question);
+  try {
+    const collected = await qC.channel.awaitMessages(filter, {
+      max: 1,
+      time: limit,
+      errors: ["time"],
+    });
+    return collected.first().content;
+  } catch (e) {
+    return false;
+  }
+}
 module.exports = class Link extends Command.Command {
   constructor(client) {
     super(client, {
@@ -13,11 +27,6 @@ module.exports = class Link extends Command.Command {
         id: message.guild.id,
       });
       
-      let description = settings.staff.filter(x => message.guild.members.includes(x)).map(x => message.guild.members.cache.get(x).username).join(", ");
-    message.channel.send(
-      new Discord.MessageEmbed()
-        .setTitle(`${message.guild.name}'s Staff Team`)
-        .setDescription(description)
-    );
-  }
+      message.author.send
+    }
 };
